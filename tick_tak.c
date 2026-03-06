d9f4df874aee354846ba00462beb8ffc3a4986b4
PoulWhite <belovodsky.pasha@gmail.com>
2026-03-06 15:57:59 +0500

    база вывода изображения готова

--- tick_tak.c
+++ tick_tak.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 char GameFeeld[3][3]{{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+char Buffer[64]{};
 
 void FillaAll(char symbol){
     for(int y = 0; y<3; y++){
@@ -19,7 +20,45 @@ void DrawGame(){
     }
 }
 
+int GetPointX(){
+    char X = 0;
+    scanf("%c", &X);
+    int x = 0;
+    if(X == 'A' || X == 'a') x = 0;
+    else if(X == 'B' || X == 'b') x = 1;
+    else if(X == 'C' || X == 'c') x = 2;
+    else x = 5;
+    return x;
+}
+
+int GetPointY(){
+    char Y = 0;
+    scanf("%c", &Y);
+    int y = 0;
+    if(Y == '1') y = 0;
+    else if(Y == '2') y = 1;
+    else if(Y == '3') y = 2;
+    else y = 5;
+    return y;
+}
+void SetX(int X, int Y){
+    GameFeeld[Y][X] = 'X';
+}
+
+void Set0(int X, int Y){
+    GameFeeld[Y][X] = '0';
+}
+
 int main(){
+   /* int X = GetPointX();
+    int Y = GetPointY();
+    printf("%d %d\n", X, Y); */
     FillaAll('.');
     DrawGame();
+    printf("________\n\n");
+    Set0(0 , 1);
+    SetX(1, 0);
+    DrawGame();
+
+    return 0;
 }
